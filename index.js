fc4c716ab93573a783a676aa2b802082aa3d07ab
Martino Lessio <mlessio@users.noreply.github.com>
2017-07-06 13:04:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 const CRI = require('chrome-remote-interface')
 const ChromeLauncher = require('chrome-launcher')
 
-const go = async () => {
+const go = async() => {
   const Chrome = await ChromeLauncher.launch({
-    // chromeFlags: ['--headless'],
-    port: 9222
+    chromeFlags: ['--headless'],
+    port: 9222,
+    chromePath: '/usr/bin/google-chrome-unstable'
   })
 
   const devtools = await CRI()
-  const { Page, Network } = devtools
+  const {
+    Page,
+    Network
+  } = devtools
 
   await Promise.all([
     Page.enable(),
     Network.enable()
   ])
 
-  await Network.enableRequestInterception({ enabled: true })
+  await Network.enableRequestInterception({
+    enabled: true
+  })
 
-  await Page.navigate({ url: 'https://calibreapp.com' })
+  await Page.navigate({
+    url: 'http://www.repubblica.it'
+  })
 
-  devtools.on('event', message => {
-    if (message.method === 'Network.requestIntercepted') console.log('intercepted', message)
+  devtools.on('event', event => {
+    if (event.method === 'Network.requestIntercepted') {
+      Network.continueInterceptedRequest({
+        "interceptionId": event.params.interceptionId
+      });
+      console.log('intercepted!');
+
+    }
   })
 
   Page.loadEventFired(() => Chrome.kill())
